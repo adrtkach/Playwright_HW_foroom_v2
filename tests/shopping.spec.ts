531f121af40f52b25cc490fc7808fd3e0e592ed3
adrtkach <adr.tkachh@gmail.com>
2022-05-04 22:01:47 +0300

    Update shopping.spec.ts

--- tests/shopping.spec.ts
+++ tests/shopping.spec.ts
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker';
 import { CartItems } from '../pages/cartItems';
 
 const productName = 'Кавомашина NIVONA CafeRomatica NICR 790';
-const buyerPhone = '980980980';
+//const buyerPhone = '980980980';
 const buyerEmail = 'email@test.com';
 const buyerName = 'Anna';
 const buyerSurname = 'Maria';
